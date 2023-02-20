21a5a3626a1c343c89da0275f0dad29db3b73dd1
hecmarla <79139578+hecmarla@users.noreply.github.com>
2023-02-20 15:03:26 +0100

    Create smile_normalizer.py

---
+++ smile_normalizer.py
@@ -0,0 +1,15 @@
+from rdkit import Chem
+from rdkit.Chem import Descriptors
+from Filtro.standardizer import Standardizer
+
+"This function normalizes all the SMILE in the same Canon SMILE format."
+def smile_normalizer(smile):
+    a = Standardizer()
+    mol = list()
+    for i in range(len(smile)):
+        m = Chem.MolFromSmiles(smile[i])
+        Chem.SanitizeMol(m)
+        m = a.sequence_rr_legacy(m)
+        mol.append(m)
+        smile[i] = Chem.CanonSmiles(Chem.MolToSmiles(m))
+    return smile, mol
