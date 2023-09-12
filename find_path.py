266716687566eb898b6101878d60d4e0b7e89375
hecmarla <79139578+hecmarla@users.noreply.github.com>
2023-09-12 11:06:07 +0200

    Create find_path.py

---
+++ find_path.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import networkx as nx
+
+def find_path(G, ini, fin):
+  path = []
+  for i in range(len(ini)):
+    for j in range(len(fin)):
+      if G.has_node(ini[i]) and G.has_node(fin[j]):
+        if nx.has_path(G, ini[i], fin[j]:
+          path.append([i,j])
+  return path
+
+def generate_path(G, ini, fin, limit=100):
+  list_paths = []
+  a = nx.shortest_simple_paths(G, ini, fin)
+  for i in a:
+    list_paths.append(i)
+    if len(list_paths) > (limit -1):
+      break
+  df = pd.DataFrame(list_paths)
+  df_len = len(df.iloc[-1])
+  keys = list(map(chr, range(65,91)))
+  df.colums = keys[0:df_len]
